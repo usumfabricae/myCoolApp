01bd2217cfb7380885bd88204f5397dc368b6ddc
EC2 Default User <ec2-user@ip-172-31-33-249.ec2.internal>
2024-09-20 15:28:41 +0000

    new file:   Gemfile

---
+++ index.js
@@ -0,0 +1,12 @@
+import { AppRegistry, Platform, Text } from 'react-native';
+import { App } from './src/App';
+import { AndroidAuto, RealAndroidAuto, AndroidAutoModule } from './src/AndroidAuto';
+
+if (Platform.OS === 'android') {
+  AppRegistry.registerRunnable('AndroidAuto', AndroidAutoModule);
+  AppRegistry.registerComponent('RNCarPlayScene', () => AndroidAuto);
+  // AppRegistry.registerRunnable('androidAuto', AndroidAutoModule);
+} else {
+  AppRegistry.registerComponent('Example', () => App);
+  AppRegistry.registerComponent('RNCarPlayScene', () => App);
+}
